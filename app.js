9459082db54e1b53b6b0f57ebf706e90bc879f12
sharpslice <davidlein1440p@gmail.com>
2025-05-16 15:40:38 -0700

    feat: added index route alongside setting up ejs view to display user messages

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","ejs");
 
+const indexRouter = require("./route/index")
+
+
+
+app.use("/",indexRouter);
 
 const PORT = 3000;
 app.listen(PORT, ()=>{
